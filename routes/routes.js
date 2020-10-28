6263dddc28c1ad360e5112d041f085beb50e639b
Thejasvi K S <40022890+thejasvi-ks@users.noreply.github.com>
2020-10-28 05:46:15 +0530

    Updated Routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const controller = require("../controllers/controller");
+
+const controller = require("../controllers/Controller");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+console.log("Reached here");
 
-router.get("/get-data", controller.getDetails);
+router.get("/disease", controller.getDisease);
+
+router.post("/disease", controller.putDisease);
+
+router.get("/diseaseage", controller.getDiseaseAge);
+
+router.post("/resident", controller.putResident);
+
+router.get("/getResidents", controller.getResidents);
+
+router.post("/putResident", controller.putResident);
+
+router.get("/statediseaseage", controller.getStateDiseaseAge);
+
+router.post("/state", controller.putState);
 
 module.exports = router;
